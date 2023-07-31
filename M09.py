4c9365b7067f69aa88b35bde9a2a35d777f977f8
kaleigh315 <77708568+kaleigh315@users.noreply.github.com>
2023-07-31 07:54:10 -0700

    Add files via upload

---
+++ M09.py
@@ -0,0 +1,8 @@
+
+def summation_nums(a,b):
+    """This function prints the sum of two numbers."""
+    c = a + b
+    print(c)
+    return c
+
+summation_nums(2,3)
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+    name='mypackagedir',
+    version='1.0.0',
+    url='https://github.com/kaleigh315/mypackage',
+    author='Kaleigh OHara',
+    author_email='ear3cg@virginia.edu',
+    description='Description of my package',
+    license='MIT'
+    packages=['M09'],    
+    install_requires=['numpy >= 1.11.1', 'matplotlib >= 1.5.1'],
+)
